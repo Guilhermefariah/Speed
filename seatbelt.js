f51b4e0de9e0098435c57c08f63a992482c526ea
Guilherme Faria <guilhermefaria321@gmail.com>
2023-12-15 14:54:34 -0300

    first commit
    Javascript and HTML

---
+++ seatbelt.js
@@ -0,0 +1,7 @@
+let speed = 78.2
+console.log(`The speed of your car is ${speed}`)
+if (speed > 60){
+    console.log('you exceeded the speed, FINED')
+
+}
+console.log('always drive with your seat belt')
